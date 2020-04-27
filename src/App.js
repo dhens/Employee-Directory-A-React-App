0d0ff98ed59ef5f37c3656af4f1b12fee93f8789
Dawson Hensel <dawsonhensel@gmail.com>
2020-04-27 14:08:35 -0700

    now renders mock table and searchform components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./components/Dashboard";
 import SearchForm from "./components/SearchForm";
 import Wrapper from "./components/Wrapper";
+import SearchResults from "./components/Dashboard";
 import './App.css';
 
 function App() {
   return (
     <div>
-      <SearchForm />
+    <SearchForm />
+      <Dashboard />
     </div>
   );
 }
